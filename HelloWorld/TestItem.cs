b76c6a9dcafa3a338fa09cf72b229934b65befa0
AsakuraRin <lanyi0fsf@163.com>
2018-08-08 14:52:43 +0800

    第二次提交

---
+++ HelloWorld/TestItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class TestItem
+    {
+        int firstInfo;
+        double secondInfo;
+
+        public TestItem()
+        {
+            this.FirstInfo = 0;
+            this.SecondInfo = 2.3;
+        }
+
+        public TestItem(int firstInfo, double secondInfo)
+        {
+            this.FirstInfo = firstInfo;
+            this.SecondInfo = secondInfo;
+        }
+
+        public int FirstInfo { get => firstInfo; set => firstInfo = value; }
+        public double SecondInfo { get => secondInfo; set => secondInfo = value; }
+
+        public void Dis()
+        {
+            Console.WriteLine("{0}+{1}",firstInfo,secondInfo);
+        }
+    }
+}
